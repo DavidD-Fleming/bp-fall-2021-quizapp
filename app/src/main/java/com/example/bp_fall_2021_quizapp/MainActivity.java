c0d7c188db588e18f8062fdad77d058cdc6620d2
David <DavidDong5252@gmail.com>
2021-11-11 15:21:32 -0500

    Added start button functionality.

--- app/src/main/java/com/example/bp_fall_2021_quizapp/MainActivity.java
+++ app/src/main/java/com/example/bp_fall_2021_quizapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bp_fall_2021_quizapp;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     // Put class variables up here
     // Best practice is to make them private (can only be accessed within the class, or using getters/setters)
     // Each UI component that you want to reference needs a variable
+    private EditText userNameInput;
 
     /**
      * Method used to start an activity. It's the first method to run when the
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // obtain user's name using findViewById
+        userNameInput = findViewById(R.id.editTextTextPersonName);
 
     }
 
@@ -33,14 +36,21 @@ public class MainActivity extends AppCompatActivity {
      */
     public void startQuiz(View view){
         // set name variable every time user clicks "start"
+        String inputtedUserName = userNameInput.getText().toString();
 
         // If the name field is empty, prompt user to enter name
+        if (inputtedUserName.isEmpty()) {
+            Toast.makeText(getBaseContext(), "Please input your name!", Toast.LENGTH_SHORT).show();
+        } else {
+            // If user has entered name, begin quiz
+            Intent intent = new Intent(this, QuizQuestionActivity.class);
+            startActivity(intent);
+            finish(); // close current activity
+        }
 
-        // If user has entered name, begin quiz
 
-        Intent intent = new Intent(this, QuizQuestionActivity.class);
-        startActivity(intent);
-        finish(); // close current activity
+
+
         }
 
     }
